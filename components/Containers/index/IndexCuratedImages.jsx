1346205ea306b5919ebd1a4b158db747dbf2bb10
Enza <79910926+enzadevs@users.noreply.github.com>
2023-09-30 20:59:27 +0500

    finished alignment of index curated images

--- components/Containers/index/IndexCuratedImages.jsx
+++ components/Containers/index/IndexCuratedImages.jsx
@@ -1,5 +1,8 @@
 import { getCuratedPhotos } from 'modules/pexels_config'
 import ImageWrapper from '../ImageWrapper'
+import Image from 'next/image'
+import Link from 'next/link'
+import FlowerImage from '/public/images/close-up-gladiolus-flower-details.jpg'
 
 export default async function IndexCuratedImages(){
     let currentPage = 0
@@ -17,8 +20,17 @@ export default async function IndexCuratedImages(){
                     />
                 )
             })}
-            <div className=''>
-                Show 
+            <div className='show-more-div-container centre radius-small'>
+                <Image
+                    className='flower-image radius-small'
+                    src={FlowerImage}
+                    alt='collage image of random photos'
+                    height={700}
+                    width={460}
+                ></Image>
+                <Link href='/en/curated' className='next-link more-curated-link hw100 centre radius-small transition-fast'>
+                    More curated photos
+                </Link>
             </div>
         </div>
     )

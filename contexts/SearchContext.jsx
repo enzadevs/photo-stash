5ef1767f6d0f86b93beed4c8766698e2e408d86f
Enza <79910926+enzadevs@users.noreply.github.com>
2023-10-03 19:24:16 +0500

    back up search

--- contexts/SearchContext.jsx
+++ contexts/SearchContext.jsx
@@ -2,7 +2,7 @@
 
 import { createClient } from 'pexels'
 const client = createClient('DvgO6rCGv7xwho7Jbm2fKH7iCnS91mpoVVTHub7DJl831NJZ1YIKkZlo')
-import { createContext,useState,useRef,useEffect } from 'react'
+import { createContext,useState,useRef} from 'react'
 
 export const ResultsContext = createContext()
 export const InputTextValue = createContext()
@@ -13,22 +13,15 @@ export default function SearchResultsProvider({children}){
     const [ currentPage,setCurrentPage ] = useState(1)
     const [ perPageImages,setPerPageImages ] = useState(5)
 
-    // const handleFetchPhotos = async() =>{
-    //     const inputTextValue = inputTextRef.current.value
-    //     const results = await client.photos.search({ query: inputTextValue, page: currentPage ,per_page: perPageImages })
-    //     setResultsArray(results)
-    // }
+    const handleFetchPhotos = async() =>{
+        const inputTextValue = inputTextRef.current.value
+        const results = await client.photos.search({ query: inputTextValue, page: currentPage ,per_page: perPageImages })
+        setResultsArray(results)
+    }
 
     const loadMorePhotos = async() =>{
         setPerPageImages(currentPhotos => currentPhotos + 5)
     }
-
-    useEffect(() => {
-        const getSearchQueryPhotos = async () => {
-        const inputTextValue = inputTextRef.current.value
-        const results = await client.photos.search({ query: inputTextValue, page: currentPage ,per_page: perPageImages })
-        setResultsArray(results)
-    }},[])
 
     return (
         <ResultsContext.Provider value={{resultsArray,loadMorePhotos,perPageImages}}>

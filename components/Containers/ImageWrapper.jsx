4387d1ab1cef7fc62fd640a0e14065605f97c4e6
Enza <79910926+enzadevs@users.noreply.github.com>
2023-10-13 19:37:54 +0500

    Updated toast element

--- components/Containers/ImageWrapper.jsx
+++ components/Containers/ImageWrapper.jsx
@@ -6,12 +6,13 @@ import dynamic from 'next/dynamic'
 import { favouritePhotosStore } from 'app/en/favourites/page'
 import JsFileDownloader from 'js-file-downloader'
 import {BiSolidHeart,BiLinkExternal,BiSolidUser,BiDownload} from 'react-icons/bi'
+import ThrowToast from 'components/Functions/Toaster'
 const CustomImage = dynamic(() => import('./CustomImage'))
 
 export default function ImageWrapper({photoObj,src,alt,photographer,quality,originalImg,photostash_url}){
     const updatePhotoObject = favouritePhotosStore(state => state.updatePhotoObject)
     const addPhotoToLiked = favouritePhotosStore(state => state.addPhotoToLiked)
-
+    
     return (
         <div className='photo-wrapper radius-small' >
             <CustomImage
@@ -50,6 +51,7 @@ export default function ImageWrapper({photoObj,src,alt,photographer,quality,orig
                                 timeout: 75000,
                                 forceDesktopMode: true
                             })
+                            ThrowToast()
                         }}
                         className='next-link download-icon-wrapper radius-small transition-fast centre'
                     ><BiDownload className='image-icons'/></span>
---
+++ components/Functions/Toaster.jsx
@@ -0,0 +1,19 @@
+import 'styles/components/toast.css'
+
+import toast from 'react-simple-toasts'
+
+export default function ThrowToast(){
+    const taskPromise = new Promise((resolve) => setTimeout(resolve, 1250))
+        const taskToast = toast('Started downloading...', {
+            className: 'my-toast centre radius-small',
+            loading: taskPromise,
+            duration: Infinity,
+        })
+
+        taskPromise.then(()=>{
+            taskToast.update({
+            message: 'Finished download',
+            duration: 1250
+        })
+    })
+}

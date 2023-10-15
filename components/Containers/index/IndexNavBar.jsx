9a4d41d613e0ca806a1967aa950ff5c1b547fb4a
Enza <79910926+enzadevs@users.noreply.github.com>
2023-10-15 21:51:37 +0500

    Fixed index nav bar routing

--- components/Containers/index/IndexNavBar.jsx
+++ components/Containers/index/IndexNavBar.jsx
@@ -9,7 +9,7 @@ export default function IndexNavBar(){
             <Link className='company-header-link next-link transition-fast centre radius-small' href='/'>
                 <h1>Photo</h1><h1 className='stash-txt'>Stash</h1>
             </Link>
-            <Link href='/en/favourites' className='index-heart-wrapper next-link centre radius-small transition'>
+            <Link href='/core/favourites' className='index-heart-wrapper next-link centre radius-small transition'>
                 <BiSolidHeart className='heart-icon'/>
             </Link>
         </nav>

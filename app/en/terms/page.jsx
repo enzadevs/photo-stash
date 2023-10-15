72a3ad5ff448f5935baeed6330b60a3280bae5cb
Enza <79910926+enzadevs@users.noreply.github.com>
2023-10-15 21:42:03 +0500

    Finished terms page

--- app/en/terms/page.jsx
+++ app/en/terms/page.jsx
@@ -1,14 +1,15 @@
+import './terms.css'
 import Link from 'next/link'
 
 export default function TermsPage(){
     return (
         <div className='terms-and-policies-page margin-auto-1440 centre'>
-            <h3 className='terms-header'>Welcome to Photo Stash.</h3>
+            <h3 className='terms-header radius-small'>Welcome to Photo Stash.</h3>
             <span className='terms-container radius-small'>
                 <p>Terms of usage:</p>
-                <li>All photos you will find on this website are from Pexels API. You can find out more <Link href='https://www.pexels.com/api/documentation/?language=javascript' className='next-link term-links' target='_blank'>here</Link>.</li>
+                <li>All photos on this website are from Pexels. You can find out more <Link href='https://www.pexels.com/api/documentation/?language=javascript' className='next-link term-links' target='_blank'>here</Link>.</li>
                 <li>If you are going to use any photo from this website, please make sure to set source of image (aka Pexels url) in your project/video/gallery. You can find all needed information in Photo page.</li>
-                <li>By default, download quality is 4K. If you need to download image in original quality, please go to Pexels website. Link will be provided in Photo page.</li>
+                <li>By default, download quality is 2K. If you need to download image in original quality, please go to Pexels website. Link will be provided in Photo page.</li>
             </span>
         </div>
     )

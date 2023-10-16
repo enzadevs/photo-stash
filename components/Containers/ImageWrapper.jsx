bad68bb25f4546f13504f4c477af1b2d9cf1b8d0
Enza <79910926+enzadevs@users.noreply.github.com>
2023-10-16 22:44:55 +0500

    Updated and finished image wrapper

--- components/Containers/ImageWrapper.jsx
+++ components/Containers/ImageWrapper.jsx
@@ -2,36 +2,36 @@
 
 import 'styles/components/image-wrapper.css'
 import Link from 'next/link'
+import CustomImage from './CustomImage'
+import ThrowToast from 'components/Functions/Toaster'
+import StartDownload from 'components/Functions/DownloadPhoto'
+import { useRouter } from 'next/navigation'
 import { FavouritePhotosStore } from 'global_states/FavouritePhotosStore'
 import {BiSolidHeart,BiLinkExternal,BiSolidUser,BiDownload} from 'react-icons/bi'
-import ThrowToast from 'components/Functions/Toaster'
-import CustomImage from './CustomImage'
-import StartDownload from 'components/Functions/DownloadPhoto'
 
 export default function ImageWrapper({photoObj,src,alt,photographer,originalImg,photostash_url}){
     const updatePhotoObject = FavouritePhotosStore(state => state.updatePhotoObject)
     const addPhotoToLiked = FavouritePhotosStore(state => state.addPhotoToLiked)
+    const router = useRouter()
     
     return (
         <div className='photo-wrapper radius-small' >
-            <CustomImage
-                src={src}
-                alt={alt}
-            />
-            <span className='image-buttons hw100 transition-fast radius-small'>
+            <CustomImage src={src} alt={alt}/>
+            <span className='image-buttons transition-fast hw100 radius-small'>
+            <span className='layer-space hw100 radius-small' onClick={()=> router.push(`/core/photo/${photostash_url}`)}></span>
                 <div className='top-right-buttons'>
-                    <span 
+                    <span
+                        className='heart-icon-wrapper centre transition-fast radius-small ' 
                         onClick={()=>{
                             updatePhotoObject(photoObj)
                             addPhotoToLiked()
-                        }}
-                        className='heart-icon-wrapper radius-small transition-fast centre'>
+                        }}>
                         <BiSolidHeart className='image-icons'/>
                     </span>
                     <Link 
-                        href={`/en/photo/${photostash_url}`}
+                        href={`/core/photo/${photostash_url}`}
                         target='_blank'
-                        className='next-link image-icons-wrapper radius-small transition-fast centre'>
+                        className='image-icons-wrapper next-link centre transition-fast radius-small'>
                         <BiLinkExternal className='image-icons'/>
                     </Link>
                 </div>
@@ -41,6 +41,7 @@ export default function ImageWrapper({photoObj,src,alt,photographer,originalImg,
                     </span>
                     <p className='photographer-name'>{photographer}</p>
                     <span
+                        className='next-link download-icon-wrapper centre transition-fast radius-small '
                         onClick={()=>{
                             StartDownload(
                                 {
@@ -50,7 +51,6 @@ export default function ImageWrapper({photoObj,src,alt,photographer,originalImg,
                             )
                             ThrowToast()
                         }}
-                        className='next-link download-icon-wrapper radius-small transition-fast centre'
                     ><BiDownload className='image-icons'/></span>
                 </div>
             </span>

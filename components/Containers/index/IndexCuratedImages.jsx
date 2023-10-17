27c5eb610c71df8aefc26a507a4645b3a72f1a45
Enza <79910926+enzadevs@users.noreply.github.com>
2023-10-17 16:52:05 +0000

    Update IndexCuratedImages.jsx

--- components/Containers/index/IndexCuratedImages.jsx
+++ components/Containers/index/IndexCuratedImages.jsx
@@ -1,37 +1,41 @@
-'use client'
+"use client";
 
-import { v4 as uuidv4 } from 'uuid'
-import { useEffect } from 'react'
-import { useCuratedPhotosStore } from 'global_states/CuratedPhotosStore'
-import ImageWrapper from '../ImageWrapper'
+import { v4 as uuidv4 } from "uuid";
+import { useEffect } from "react";
+import { useCuratedPhotosStore } from "global_states/CuratedPhotosStore";
+import ImageWrapper from "../ImageWrapper";
 
-export default async function IndexCuratedImages(){
-    const curatedPhotosArray = useCuratedPhotosStore(state => state.curatedPhotosArray)
-    const minCuratedPhotos = useCuratedPhotosStore(state => state.minCuratedPhotos)
+export default async function IndexCuratedImages() {
+  const curatedPhotosArray = useCuratedPhotosStore(
+    (state) => state.curatedPhotosArray,
+  );
+  const minCuratedPhotos = useCuratedPhotosStore(
+    (state) => state.minCuratedPhotos,
+  );
 
-    useEffect(() =>{
-        try{
-            minCuratedPhotos()
-        } catch(error){
-            throw new Error(console.log('Failed to fetch images.'))
-        }
-    },[])
+  useEffect(() => {
+    try {
+      minCuratedPhotos();
+    } catch (error) {
+      throw new Error(console.log("Failed to fetch images."));
+    }
+  }, []);
 
-    return (
-        <div className='columns-container margin-auto-1440'>
-            {curatedPhotosArray?.map( photo =>{
-                return(
-                    <ImageWrapper
-                        key={uuidv4()}
-                        photoObj={photo}
-                        photostash_url={photo.id}
-                        originalImg={photo.src.original}
-                        photographer={photo.photographer}
-                        src={photo.src.large2x + `?auto=compress&height=800&width=500`}
-                        alt={photo.alt}
-                    />
-                )
-            })}
-        </div>
-    )
+  return (
+    <div className="columns-container margin-auto-1440">
+      {curatedPhotosArray?.map((photo) => {
+        return (
+          <ImageWrapper
+            key={uuidv4()}
+            photoObj={photo}
+            photostash_url={photo.id}
+            originalImg={photo.src.original}
+            photographer={photo.photographer}
+            src={photo.src.large + `?auto=compress&height=800&width=500`}
+            alt={photo.alt}
+          />
+        );
+      })}
+    </div>
+  );
 }
